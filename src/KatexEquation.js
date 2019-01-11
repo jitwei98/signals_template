002591136df4ba0c480799c346fd85ec9656fc68
jitwei98 <limjitwei@gmail.com>
2019-01-11 18:19:52 +0800

    KatexEquation.js: add <hr>

--- src/KatexEquation.js
+++ src/KatexEquation.js
@@ -3,13 +3,14 @@ import './App.css';
 import 'katex/dist/katex.min.css';
 import { InlineMath, BlockMath } from 'react-katex';
 
-export default class LatexEquation extends Component {
+export default class KatexEquation extends Component {
 
   render() {
      return (
             <h3>
                 <InlineMath math={`\\int_0^\\infty x^2 dx`}/>
                 <BlockMath math={`\\int_0^\\infty x^2 dx`}/>
+                <hr />
             </h3>
         );
   }
