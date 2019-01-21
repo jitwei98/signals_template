a4e71d700c84fac889e96e98c8d9569368bf1509
LIU-TIANHANG <you@example.com>
2019-01-21 20:59:31 +0800

    added

--- src/formula/Formula.js
+++ src/formula/Formula.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Route } from "react-router-dom";
 import 'katex/dist/katex.min.css';
 import { Switch, Route } from "react-router-dom";
 import EulerFormula from './chapter1/EulerFormula';
